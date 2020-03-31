3bb1b35eaea98efcfdb4fcdb232e5f6fcbeebd93
RoyaLerino <49786797+RoyaLerino@users.noreply.github.com>
2020-03-31 21:30:42 +0100

    Audio working for 1 track now
    I added the ability to play audio now on each page, only plays one track just now and working on way for it to play other tracks that are listed in raw folder in res.

--- app/src/main/java/com/uws/project/DetailsActivity.java
+++ app/src/main/java/com/uws/project/DetailsActivity.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,12 +23,15 @@ public class DetailsActivity extends AppCompatActivity {
     TextView textTitle, textArtist, textLyrics;
     ImageView imageAlbum;
     ArrayList<String> settingsObject;
+    MediaPlayer player;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        context= this;
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -63,6 +69,39 @@ public class DetailsActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    public void play (View v){
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.blindinglights);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
+    }
+    public void pause (View v){
+        if (player != null) {
+            player.pause();
+        }
+    }
+    public void stop (View v) {
+        stopPlayer();
+    }
+    private void stopPlayer() {
+        if (player != null){
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
     }
 
     public void lyricsAppearance() {
