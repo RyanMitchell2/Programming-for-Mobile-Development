6f3ed43d4df384ae924ed18bb9b833db0370a36e
chrisd848 <chris.online.6563@gmail.com>
2020-04-03 16:36:26 +0100

    Comment Functionality

--- app/src/main/java/com/uws/project/DetailsActivity.java
+++ app/src/main/java/com/uws/project/DetailsActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -23,6 +22,7 @@ import java.util.Objects;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    // My code
     TextView textTitle, textArtist, textLyrics;
     ImageView imageAlbum;
     ArrayList<String> settingsObject, comments_body;
